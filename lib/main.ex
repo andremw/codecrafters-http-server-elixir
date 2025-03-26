8deebdce1b6432254ce9b8d9fd48ba5b0e046dbb
andremw <mazoni.andre@gmail.com>
2025-03-26 20:25:58 +1030

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -14,9 +14,40 @@ defmodule Server do
     # Since the tester restarts your program quite often, setting SO_REUSEADDR
     # ensures that we don't run into 'Address already in use' errors
     {:ok, socket} = :gen_tcp.listen(4221, [:binary, active: false, reuseaddr: true])
+
+    listen_loop(socket)
+  end
+
+  def listen_loop(socket) do
     {:ok, client} = :gen_tcp.accept(socket)
 
-    :gen_tcp.send(client, "HTTP/1.1 200 OK\r\n\r\n")
+    {:ok, request} = :gen_tcp.recv(client, 0)
+
+    response = handle_request(request)
+    IO.puts("\nReceived request: #{request}")
+
+    :gen_tcp.send(client, response)
+    :gen_tcp.close(client)
+
+    listen_loop(socket)
+  end
+
+  defp handle_request(request) do
+    request
+    |> parse
+    |> format_response
+  end
+
+  defp parse(request) do
+    [method, path, _] = request |> String.split("\r\n") |> Enum.at(0) |> String.split(" ")
+    %{method: method, path: path}
+  end
+
+  defp format_response(%{ method: "GET", path: "/" }) do
+    "HTTP/1.1 200 OK\r\n\r\n"
+  end
+  defp format_response(_conv) do
+    "HTTP/1.1 404 Not Found\r\n\r\n"
   end
 end
 
